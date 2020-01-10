3d6f3031b7eebdd5a568a70dc455bc5535c713d3
Juan Pablo Fernandez Tubello <juanfernatubello@gmail.com>
2020-01-09 21:54:25 -0300

    Add files via upload

---
+++ twitterconfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+    consumer_key: 'Your credentials',
+    consumer_secret: 'Your credentials',
+    access_token: 'Your credentials',
+    access_token_secret: 'Your credentials',
+    timeout_ms: 'Your credentials',
+    strictSSL: 'Your credentials',
+}
