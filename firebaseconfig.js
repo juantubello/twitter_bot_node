ea698cf3cce2623f8eeced797aad584437ed21c0
Juan Pablo Fernandez Tubello <juanfernatubello@gmail.com>
2020-01-09 21:53:11 -0300

    Add files via upload

---
+++ firebaseconfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+        type: "Your credentials",
+        project_id: "Your credentials",
+        private_key_id: "Your credentials",
+        private_key: "Your credentials",
+        client_email: "Your credentials",
+        client_id: "Your credentials",
+        auth_uri: "Your credentials",
+        token_uri: "Your credentials",
+        auth_provider_x509_cert_url: "Your credentials",
+        client_x509_cert_url: "Your credentials"
+}
